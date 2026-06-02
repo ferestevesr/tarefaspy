ff6cb2626ccb9840f67fbbb70dea86d73c6f41c7
ferestevesr <fernandovinirocha@gmail.com>
2026-06-02 15:13:42 -0300

    Adicionando rota inicial

--- projetoti102/routes.py
+++ projetoti102/routes.py
@@ -1,5 +1,9 @@
 from projetoti102 import app
 from flask import render_template, url_for
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
