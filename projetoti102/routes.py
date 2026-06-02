f38c8b109a34d34619a007c7597b73d4181338a0
ferestevesr <fernandovinirocha@gmail.com>
2026-06-02 14:23:28 -0300

    first commit

---
+++ projetoti102/routes.py
@@ -0,0 +1,9 @@
+from projetoti102 import app
+from flask import render_template, url_for
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
