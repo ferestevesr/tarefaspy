44b87b3f13bb8d655a819b7318c239842aeabec0
ferestevesr <fernandovinirocha@gmail.com>
2026-06-10 16:32:39 -0300

    atualização

--- projetoti102/routes.py
+++ projetoti102/routes.py
@@ -1,17 +1,23 @@
-from flask_login import login_required
-
 from projetoti102 import app
 from flask import render_template, url_for
+from flask_login import login_required
+from projetoti102.forms import FormLogin, FormCriarConta
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('homepage.html')
+    formlogin=FormLogin()
+    return render_template('homepage.html', form=formlogin)
 
+@app.route('/criarconta', methods=['GET', 'POST'])
+def criarconta():
+    formCriarConta = FormCriarConta()
+    return render_template('criarConta.html', form=formCriarConta)
 
 @app.route('/perfil/<usuario>')
 @login_required
 def perfil(usuario):
+    formCriarConta = FormCriarConta()
     return render_template('perfil.html', usuario=usuario)
 
 
