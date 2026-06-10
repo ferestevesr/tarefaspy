bf86e72303beff24f0a48eaabab9e54ac957dcc3
ferestevesr <fernandovinirocha@gmail.com>
2026-06-10 15:17:42 -0300

    atualização

--- projetoti102/__init__.py
+++ projetoti102/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ti102.db'
-database = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'uTGVmAbzg4A8AjZ5oX7wD0UD4OK9evVP_YVzJGp_WXg'
 
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'home'
 from projetoti102 import routes, models
--- projetoti102/routes.py
+++ projetoti102/routes.py
@@ -1,13 +1,17 @@
+from flask_login import login_required
+
 from projetoti102 import app
 from flask import render_template, url_for
+
+
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
-@app.route('/homepage')
-def homepage():
-    return render_template('homepage.html')
 
-@app.route('/perfil')
-def perfil():
-    return render_template('perfil.html')
+@app.route('/perfil/<usuario>')
+@login_required
+def perfil(usuario):
+    return render_template('perfil.html', usuario=usuario)
+
+
