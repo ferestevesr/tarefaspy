5f8dc88dc849e4e373666924712a3989427776d3
ferestevesr <fernandovinirocha@gmail.com>
2026-06-17 14:28:02 -0300

    Atualização no routes, adição da rota logout

--- projetoti102/routes.py
+++ projetoti102/routes.py
@@ -2,7 +2,7 @@ from gunicorn.systemd import sd_notify
 
 from projetoti102 import app, bcrypt, database
 from flask import render_template, url_for, redirect
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from projetoti102.forms import FormLogin, FormCriarConta
 from projetoti102.models import Usuario
 
@@ -13,7 +13,22 @@ from projetoti102.models import Usuario
 @app.route('/', methods=['GET', 'POST'])
 def home():
     formlogin = FormLogin()
+
+    if formlogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+
+
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
+            login_user(usuario, remember=True)
+            return redirect(url_for('perfil', usuario=usuario.username))
+
     return render_template('homepage.html', form=formlogin)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/criarconta', methods=['GET', 'POST'])
 def criarconta():
